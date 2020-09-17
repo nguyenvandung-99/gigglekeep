cecb70690af140d6b508aebcbf284af9994669a7
nguyenvandung-99 <68967018+nguyenvandung-99@users.noreply.github.com>
2020-09-17 05:24:51 +0200

    change app route

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -12,17 +12,14 @@ export default class HomeScreen extends Component {
         <div className="content">
           <div className="navigation">
             <div className="navigation-option">
-              <Link to="/home">Your Notes</Link>
+              <Link to="/">Your Notes</Link>
             </div>
             <div className="navigation-option">
               <Link to="/bin">Bin</Link> 
             </div>
           </div>
           <div className="main">
-            <Route path="/" exact>
-              <Redirect to="/home" />
-            </Route>
-            <Route path="/home" component={UserNotes} />
+            <Route path="/" component={UserNotes} />
             <Route path="/bin" component={UserBin} />
           </div>
           <footer></footer>
